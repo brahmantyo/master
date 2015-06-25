deed37a85d00570a7f11b44a4f8ad55f3cb9e1a1
brahmantyo <brahmantyo.adi@gmail.com>
2015-06-25 15:41:58 +0700

    perbaikan migration file

--- database/migrations/2015_06_24_232354_rename_table_daftarresi_to_resi.php
+++ database/migrations/2015_06_24_232354_rename_table_daftarresi_to_resi.php
@@ -20,8 +20,15 @@ class RenameTableDaftarresiToResi extends Migration {
 			$table->foreign('idresi')->references('noresi')->on('resi');
 		});*/
 		Schema::table('resi',function(Blueprint $table){
+			$table->dropForeign('daftarresi_idberangkat_foreign');
 			$table->renameColumn('idberangkat','idsjn')->change();
-			
+		});
+		Schema::rename('berangkat','suratjalan');
+		Schema::table('suratjalan',function(Blueprint $table){
+			$table->renameColumn('idberangkat','idsjn')->change();
+		});
+		Schema::table('resi',function(Blueprint $table){
+			$table->foreign('idsjn')->references('idsjn')->on('suratjalan');
 		});
 	}
 
@@ -33,15 +40,21 @@ class RenameTableDaftarresiToResi extends Migration {
 	public function down()
 	{
 		Schema::table('resi',function(Blueprint $table){
+			$table->dropForeign('resi_idsjn_foreign');
+		});
+		Schema::rename('suratjalan','berangkat');
+		Schema::table('resi',function(Blueprint $table){
 			$table->renameColumn('idsjn','idberangkat')->change();
-			
 		});		
+		Schema::table('berangkat',function(Blueprint $table){
+			$table->renameColumn('idsjn','idberangkat')->change();
+		});
 		//Schema::table('detailresi',function(Blueprint $table){
 		//	$table->dropForeign('detailresi_idresi_foreign');
 		//});
 		Schema::rename('resi','daftarresi');
-		//Schema::table('detailresi',function(Blueprint $table){
-		//	$table->foreign('idresi')->references('noresi')->on('daftarresi');
-		//});
+		Schema::table('daftarresi',function(Blueprint $table){
+			$table->foreign('idberangkat')->references('idberangkat')->on('berangkat');
+		});
 	}
 }
